f591daf5176b182d94e8712519b84f8c3be845a5
wiraphong2003 <64011212049@msu.ac.th>
2023-09-28 01:27:22 +0700

    updartas

---
+++ myapp/lib/models/Groups.g.dart
@@ -0,0 +1,16 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'Groups.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Groups _$GroupsFromJson(Map<String, dynamic> json) => Groups()
+  ..groups = (json['groups'] as List<dynamic>)
+      .map((e) => Group.fromJson(e as Map<String, dynamic>))
+      .toList();
+
+Map<String, dynamic> _$GroupsToJson(Groups instance) => <String, dynamic>{
+      'groups': instance.groups,
+    };
--- myapp/lib/models/users.g.dart
+++ myapp/lib/models/users.g.dart
@@ -6,7 +6,7 @@ part of 'users.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-Users _$UsersFromJson(Map<String, dynamic> json) => Users(users: [], Groups: [])
+Users _$UsersFromJson(Map<String, dynamic> json) => Users()
   ..users = (json['users'] as List<dynamic>)
       .map((e) => User.fromJson(e as Map<String, dynamic>))
       .toList();
